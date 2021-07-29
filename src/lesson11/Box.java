4497e904693fb7efda15767f313b5eec9ff7b9f7
RustamKZZ <kzrustam@gmail.com>
2021-07-29 13:02:11 +0300

    Homework11

---
+++ src/lesson11/Box.java
@@ -0,0 +1,40 @@
+package lesson11;
+
+import java.util.ArrayList;
+
+public class Box <T extends Fruit> {
+    private ArrayList<T> box = new ArrayList<>();
+
+    public Box() {
+    }
+
+    public float getWeight() {
+        float weight = 0.0f;
+        for (T o : box) {
+            weight += o.getWeight();
+        }
+        return weight;
+    }
+
+    public boolean compare(Box anotherBox) {
+        if (getWeight() == anotherBox.getWeight()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addTo(T fruit, int amount) {
+        for (int i = 0; i < amount; i++) {
+            box.add(fruit);
+
+        }
+    }
+
+    public void pourTo(Box<T> anotherBox) {
+        anotherBox.box.addAll(box);
+        box.clear();
+
+
+    }
+}
---
+++ src/lesson11/Fruit.java
@@ -0,0 +1,6 @@
+package lesson11;
+
+public abstract class Fruit {
+    abstract float getWeight();
+
+}
